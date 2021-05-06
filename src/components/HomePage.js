b0f90c899ca07ff04c6a8fca3f0ba130b6505ce7
Satish.Marathe <marathes_pune@yahoo.com>
2021-05-06 18:04:14 +1000

    changed home page Title

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import React from "react";
 function HomePage(){
     return(
         <div className="jumbotron">
-            <h1>Energy Adminsitration</h1>
+            <h1>Tree Survey</h1>
             <a href="/about">About</a>
         </div>
     );
