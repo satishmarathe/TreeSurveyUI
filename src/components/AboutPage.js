515aa2d70202cc007f730f0249fbca1851bb0344
Satish.Marathe <marathes_pune@yahoo.com>
2021-05-06 17:59:54 +1000

    added some more text to About page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -3,7 +3,8 @@ import React from "react";
 function AboutPage(){
     return(
         <React.Fragment>
-            This is the about page.
+            This app will provide details of Tree Surveys carried out . It will also allow users to submit their survey details as well 
+            as view edit and delete survey information
         </React.Fragment>
             
     );
