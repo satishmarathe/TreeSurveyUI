ce15ebc346dd8738d989410d4196da937c4fa9d5
Satish.Marathe <marathes_pune@yahoo.com>
2021-05-15 09:23:25 +1000

    changed header to use NavBar and show a more decent Header

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+ Navbar,
+ Nav,
+ NavDropdown,
+ Form,
+ FormControl,
+ Button
+} from "react-bootstrap";
+ 
 
-function Header(){
-    return(
-        <nav>
-            <a href="/">Home</a> | <a href="/surveys">Surveys</a> | {" "}<a href="/about">About </a>  
-        </nav>
-    )
-}
+ 
+const Header = () => {
+ const [modalShow, setModalShow] = useState(false);
+ 
+ return (
+   <Navbar bg="primary" variant="dark" expand="md">
+     <Navbar.Brand href="#home">Tree-Survey</Navbar.Brand>
+     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+     <Navbar.Collapse id="basic-navbar-nav">
+       <Nav className="mr-auto">
+         
+         
+       </Nav>
+       
+     </Navbar.Collapse>
+   </Navbar>
+ );
+};
 export default Header;
